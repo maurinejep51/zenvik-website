e301068968edcbaf2f7c6aac0257d1c8b41fc73c
Alexander Kiprop <102218687+254alexis@users.noreply.github.com>
2026-05-17 23:10:07 +0000

    "Add site configuration constants"

---
+++ main-site/src/constants/site.js
@@ -0,0 +1,8 @@
+export const siteConfig = {
+  name: "Zenvik Technologies",
+  domain: "zenviktechnologies.com",
+  portalUrl: "https://portal.zenviktechnologies.com",
+  email: "info@zenviktechnologies.com",
+  supportEmail: "support@zenviktechnologies.com",
+  tagline: "Powering Modern Businesses Through Technology",
+}
